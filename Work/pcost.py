af5d65f1acc2585641cfc5fc3d8e7f96c6e23fc7
Shruti <shrutigehlot2629@gmail.com>
2021-03-03 01:08:42 +0530

    d3

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
+
+if __name__ == '__main__':
+    Total_cost = 0.0
+
+    #1 read file 
+    #2 perform convertion and calculate the total amount 
+    with open('practical_python/practical-python/Work/Data/portfolio.csv',mode = 'rt') as file :
+        header = next(file)   #doesn't make sense for me  
+                #returns the next line in the file 
+                #used to explicitly read or skip the line
+        
+        for line in file:
+            line = line.strip().split(',')
+            Total_cost = Total_cost + (int(line[1])*float(line[2]))
+    print(Total_cost)    
+
